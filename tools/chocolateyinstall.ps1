039983d7fd01ef6af573af0b79fd5f99f18d4424
Mike Cooper <mike@equin.co.uk>
2026-05-06 13:10:43 +0100

    v1.5.12 and add iconUrl

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = $env:ChocolateyPackageName
 
-$url = 'https://github.com/firezone/firezone/releases/download/gui-client-1.5.11/firezone-client-gui-windows_1.5.11_x86_64.msi'
+$url = 'https://github.com/firezone/firezone/releases/download/gui-client-1.5.12/firezone-client-gui-windows_1.5.12_x86_64.msi'
 
 Write-Host "NOTE: This is an UNOFFICIAL, community-maintained Chocolatey package for Firezone."
 Write-Host "It is not maintained or endorsed by Firezone and may lag behind official releases."
@@ -14,7 +14,7 @@ $packageArgs = @{
 
   softwareName  = 'Firezone'
 
-  checksum      = 'EF33C1CDF1CB5F8546B85A8D901F0C1B6866661219D9E545D09854DFE1ADA406'
+  checksum      = 'EECA0F84F8CE67EE7A69FD58193F147D8B1D2A906CC172D3474335E2C62E0205'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
